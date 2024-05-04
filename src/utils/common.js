29f0377bbd882b8fe352176bbbb9f46c667b9bcd
syki66 <66syki@gmail.com>
2024-05-04 19:51:49 +0900

    feat: 숫자 포맷 기능 추가
    - 숫자 쉼표 및 소수점 2자리 처리
    - 통화 및 % 표기
    - baseURL 환경변수 추가

---
+++ src/utils/common.js
@@ -0,0 +1,29 @@
+export const addCommasAndDecimal = (number) => {
+  const numStr = number.toString();
+  const regex = /\B(?=(\d{3})+(?!\d))/g;
+
+  if (numStr.indexOf('.') !== -1) {
+    const parts = numStr.split('.');
+    parts[0] = parts[0].replace(regex, ',');
+    parts[1] = parts[1] ? parts[1].slice(0, 2) : '00';
+
+    return parts.join('.');
+  } else {
+    return numStr.replace(regex, ',');
+  }
+};
+
+export const formatNumber = (number, currency) => {
+  const currencyObject = {
+    krw: '₩',
+    usd: '$',
+    percent: '%',
+    undefined: '',
+  };
+
+  if (currency === '%') {
+    return addCommasAndDecimal(number) + '%';
+  } else {
+    return currencyObject[currency] + addCommasAndDecimal(number);
+  }
+};
