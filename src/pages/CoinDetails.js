a84f8a5701a75d22d65b47cb31d8beae5083b5ab
syki66 <66syki@gmail.com>
2024-05-05 03:33:54 +0900

    feat: coinDetail 페이지 실제 데이터로 전환 - 설명 토글 추가

--- src/pages/CoinDetails.js
+++ src/pages/CoinDetails.js
@@ -2,12 +2,20 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './CoinDetails.module.css';
+import { formatNumber } from '../utils/common';
 
 export default function CoinDetails() {
   const params = useParams();
 
   const [isLoading, setIsLoading] = useState(true);
   const [coin, setCoin] = useState({});
+  const [currency, setCurrency] = useState('krw');
+  const [description, setDescription] = useState('');
+  const [descOpen, setDescOpen] = useState(false);
+
+  const toggleDescOpen = () => {
+    setDescOpen(!descOpen);
+  };
 
   const getCoinDetail = async (id) => {
     try {
@@ -16,13 +24,27 @@ export default function CoinDetails() {
       setCoin(response.data);
       setIsLoading(false);
     } catch (error) {
-      console.log(error);
+      alert(error);
     }
   };
 
   useEffect(() => {
     getCoinDetail(params.id);
   }, []);
+
+  useEffect(() => {
+    if (coin.description) {
+      let _description = '';
+      if (coin.description.en) {
+        _description = coin.description.en;
+      }
+      if (coin.description.ko) {
+        _description = coin.description.ko;
+      }
+
+      setDescription(_description.replace(/\r\n/g, '<br>'));
+    }
+  }, [coin]);
 
   if (isLoading) {
     return <>로딩중</>;
@@ -40,9 +62,13 @@ export default function CoinDetails() {
         </section>
 
         <section className={styles.currency}>
-          <select>
-            <option>KRW 보기</option>
-            <option>USD 보기</option>
+          <select
+            onChange={(e) => {
+              setCurrency(e.target.value);
+            }}
+          >
+            <option value="krw">KRW 보기</option>
+            <option value="usd">USD 보기</option>
           </select>
         </section>
 
@@ -60,22 +86,46 @@ export default function CoinDetails() {
         <section className={styles.priceContainer}>
           <div>
             <div className={styles.currencyPrice}>
-              <div>2000000000만원</div>
-              <div>0.8%</div>
+              <div>
+                {formatNumber(
+                  coin.market_data.current_price[currency],
+                  currency
+                )}
+              </div>
+              <div>
+                {formatNumber(
+                  coin.market_data.price_change_percentage_24h_in_currency[
+                    currency
+                  ],
+                  '%'
+                )}
+              </div>
             </div>
             <div className={styles.coinPrice}>
-              <div>1.00000000000btc</div>
-              <div>0.1%</div>
+              <div>1.00000000 {coin.symbol.toUpperCase()}</div>
+              <div>
+                {formatNumber(
+                  coin.market_data.price_change_percentage_24h,
+                  '%'
+                )}
+              </div>
             </div>
           </div>
           <div>
             <div className={styles.marketCap}>
               <div>시가총액</div>
-              <div>3842374827349</div>
+              <div>
+                {formatNumber(coin.market_data.market_cap[currency], currency)}
+              </div>
             </div>
             <div className={styles.volume}>
-              <div>24시간 거래대금</div>
-              <div>9999999999999</div>
+              <div>총 거래대금</div>
+              <div>
+                {formatNumber(
+                  coin.market_data.total_volume[currency],
+                  currency
+                )}
+              </div>
             </div>
           </div>
         </section>
@@ -83,14 +133,14 @@ export default function CoinDetails() {
           <h2>가격 계산</h2>
           <div className={styles.inputContainer}>
             <div>
-              <label htmlFor="coinInput">BTC</label>
+              <label htmlFor="coinInput">{coin.symbol.toUpperCase()}</label>
               <span>
                 <input type="text" id="coinInput" />
               </span>
             </div>
             <div>↔</div>
             <div>
-              <label htmlFor="textInput">KRW</label>
+              <label htmlFor="textInput">{currency.toUpperCase()}</label>
               <span>
                 <input type="text" id="textInput" />
               </span>
@@ -98,10 +148,14 @@ export default function CoinDetails() {
           </div>
         </section>
 
-        <section className={styles.description}>
-          <div>설명보기 </div>
-          <p>{coin.description.ko}</p>
-        </section>
+        {description && (
+          <section className={styles.description}>
+            <div onClick={toggleDescOpen}>설명보기 ▼</div>
+            {descOpen && (
+              <p dangerouslySetInnerHTML={{ __html: description }} />
+            )}
+          </section>
+        )}
       </div>
     </>
   );
