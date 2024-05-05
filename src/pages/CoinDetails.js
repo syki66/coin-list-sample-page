dcc33c77d2d0a919fe4d0f8b3afbe6f0137c4306
syki66 <66syki@gmail.com>
2024-05-05 20:06:02 +0900

    feat: url 쿼리스트링 동적 제어 기능 추가 - selectbox 기능 추가 - 페이지네이션 추가 - loading 기능 추가 - 네트워크 에러처리

--- src/pages/CoinDetails.js
+++ src/pages/CoinDetails.js
@@ -62,7 +62,12 @@ export default function CoinDetails() {
       setCoin(response.data);
       setIsLoading(false);
     } catch (error) {
-      alert(error);
+      console.log(error);
+      if (error.code === 'ERR_NETWORK') {
+        alert(
+          '네트워크 오류가 발생했습니다. API를 빈번하게 호출할 경우 오류가 발생할 수 있습니다. \n\n 잠시 후 다시 이용해주세요.'
+        );
+      }
     }
   };
 
--- src/pages/CoinList.js
+++ src/pages/CoinList.js
@@ -4,27 +4,74 @@ import { formatNumber } from '../utils/common';
 import { useNavigate } from 'react-router-dom';
 
 export default function CoinList() {
+  const [isLoading, setIsLoading] = useState(true);
   const [coinList, setCoinList] = useState([]);
   const [currency, setCurrency] = useState('krw');
+  const [perPage, setPerPage] = useState(50);
+  const [listNum, setListNum] = useState(50);
 
   const navigate = useNavigate();
 
+  const handleMoreClick = () => {
+    setListNum(listNum + perPage);
+  };
+
+  useEffect(() => {
+    console.log(perPage);
+  }, [perPage]);
+
   const getCoinList = async () => {
     try {
-      const url = `${process.env.REACT_APP_BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=30&page=1&price_change_percentage=1h,24h,7d`;
+      const url = `${process.env.REACT_APP_BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${listNum}&page=1&price_change_percentage=1h,24h,7d`;
       const response = await axios.get(url);
       setCoinList(response.data);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
+      if (error.code === 'ERR_NETWORK') {
+        alert(
+          '네트워크 오류가 발생했습니다. API를 빈번하게 호출할 경우 오류가 발생할 수 있습니다. \n\n 잠시 후 다시 이용해주세요.'
+        );
+      }
     }
   };
 
   useEffect(() => {
     getCoinList();
-  }, [currency]);
+  }, [currency, listNum]);
+
+  if (isLoading) {
+    return <>로딩 중 ...</>;
+  }
 
   return (
     <>
+      <select>
+        <option>전체보기</option>
+        <option>북마크 보기</option>
+      </select>
+      <select
+        onChange={(e) => {
+          setCurrency(e.target.value);
+        }}
+      >
+        <option selected value={'krw'}>
+          KRW 보기
+        </option>
+        <option value={'usd'}>USD 보기</option>
+      </select>
+      <select
+        onChange={(e) => {
+          setPerPage(parseInt(e.target.value));
+          setListNum(parseInt(e.target.value));
+        }}
+      >
+        <option value={10}>10개 보기</option>
+        <option value={30}>30개 보기</option>
+        <option selected value={50}>
+          50개 보기
+        </option>
+      </select>
       <table>
         <thead>
           <tr>
@@ -67,6 +114,8 @@ export default function CoinList() {
           ))}
         </tbody>
       </table>
+
+      <div onClick={handleMoreClick}>+ 더보기</div>
     </>
   );
 }
