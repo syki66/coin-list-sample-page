f38e357c7c539343ed7073511cdfb3a23f52bca7
syki66 <66syki@gmail.com>
2024-07-11 23:31:22 +0900

    env: 모든 실행환경에서 msw 실행

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return;
-  }
-
   const { worker } = await import('./mocks/browser');
   return worker.start();
 }
